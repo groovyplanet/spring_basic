01bcac325796037372a067770d06a9cd8e8b3b87
groovyplanet <sneakersgroovy@gmail.com>
2024-08-01 17:49:57 +0900

    08/01 Spring Setting & Basic

---
+++ StringDI/src/main/java/ex05/javaconfig/MainClass.java
@@ -0,0 +1,14 @@
+package ex05.javaconfig;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import ex04.quiz.Computer;
+
+public class MainClass {
+	public static void main(String[] args) {
+
+		
+	
+	}
+
+}
